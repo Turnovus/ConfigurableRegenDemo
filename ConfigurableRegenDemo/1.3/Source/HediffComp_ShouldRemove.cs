a1d4932601acc8c43555f8c2b2a084fe9c13e5c1
Turnovus <dinolover075@gmail.com>
2021-07-09 13:20:34 -0400

    Added files via upload.

---
+++ ConfigurableRegenDemo/1.3/Source/HediffComp_ShouldRemove.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+using RimWorld;
+
+namespace LimitedRegen
+{
+    /// <summary>
+    /// A <c>HediffComp</c> applied to the default missing part. It allows
+    /// missing parts to be removed during the <c>HealthTick</c> without
+    /// causing errors.
+    /// </summary>
+    class HediffComp_ShouldRemove : HediffComp
+    {
+        /// <summary>
+        /// Informs developers if a <c>Hediff</c> is supposed to be removed. If
+        /// this text is visible on an active <c>Hediff</c>, something is
+        /// wrong.
+        /// </summary>
+        /// <returns>
+        /// Updated debug string.
+        /// </returns>
+        public override string CompDebugString()
+        {
+            return base.CompDebugString() + 
+                "Should be removed next tick.";
+        }
+
+        /// <summary>
+        /// This method is used to determine if a HediffComp's parent 
+        /// Hediff should be removed when it is safe to do so. It is run by
+        /// HediffWithComps.ShouldRemove. Some Hediffs,
+        /// like Hediff_MissingPart, override this method and must be patched.
+        /// </summary>
+        public override bool CompShouldRemove => true;
+    }
+}
---
+++ ConfigurableRegenDemo/1.3/Source/PatchRunner.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+using RimWorld;
+using HarmonyLib;
+
+namespace LimitedRegen
+{
+    /// <summary>
+    /// Responsible for running all of the Harmony patches on game start.
+    /// </summary>
+    [StaticConstructorOnStartup]
+    static class PatchRunner
+    {
+        /// <summary>
+        /// Constructor to run on startup, calls the method for patching.
+        /// </summary>
+        static PatchRunner()
+        {
+            DoPatchingInt("com.turnovus.configurableregen.harmonypatches");
+        }
+
+        /// <summary>
+        /// Applies all Harmony patches defined in other classes.
+        /// </summary>
+        /// <param name="id">
+        /// Package ID to help other modders identify our patches
+        /// </param>
+        private static void DoPatchingInt(string id)
+        {
+            var harmony = new Harmony(id);
+            harmony.PatchAll();
+        }
+    }
+}
